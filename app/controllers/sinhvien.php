5512ab1ae4543be84367e49269498a9716cec275
Phong <phonghaipham026@gmail.com>
2026-05-26 22:06:30 +0700

    feat: Hiển thị danh sách sinh viên

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,11 +1,45 @@
-<?php
-class sinhvien{
-    public function index(){
-        echo "Đây là trang tạo sinh viên";
-    }
-    public function create(){
-        echo "Đây là trang tạo sinh viên";
-    }
-}
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh sách sinh viên</title>
+</head>
+<body>
+    <h1>Danh sách sinh viên</h1>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 5px;
+            text-align: left;
+        }
+        th {
+            background-color: white;
+        }
+    </style>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>MSSV</th>
+                <th>Họ tên</th>
+                <th>Giới tính</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($sinhvien as $sv): ?>
+                <tr>
+                    <td><?php echo $sv['id']; ?></td>
+                    <td><?php echo $sv['MSSV']; ?></td>
+                    <td><?php echo $sv['HoTen']; ?></td>
+                    <td><?php echo $sv['GioiTinh']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
